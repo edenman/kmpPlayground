71d3fa71f08619bf4e7ad653488c1266b23dfa84
Eric Denman <edenman@users.noreply.github.com>
2020-07-29 13:18:59 +1200

    Demonstration of include+merge+id+viewbinding crash

--- app/src/main/java/com/coffeetrainlabs/kmpplayground/HomeScreenView.kt
+++ app/src/main/java/com/coffeetrainlabs/kmpplayground/HomeScreenView.kt
@@ -5,10 +5,14 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.TextView
+import android.widget.ViewAnimator
 import androidx.core.view.children
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.coffeetrainlabs.kmpplayground.databinding.DetailScreenBinding
+import com.coffeetrainlabs.kmpplayground.databinding.HomeScreenBinding
 import flow.Flow
 import kotlinx.android.parcel.Parcelize
 
@@ -17,10 +21,14 @@ object HomeScreen : Screen {
   override fun layoutResId() = R.layout.home_screen
 }
 
-class HomeScreenView(context: Context, attrs: AttributeSet?) : RecyclerView(context, attrs) {
-  init {
-    adapter = HomeScreenAdapter(context)
-    layoutManager = LinearLayoutManager(context)
+class HomeScreenView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+  private val binding by viewBinding(HomeScreenBinding::bind)
+
+  override fun onFinishInflate() {
+    super.onFinishInflate()
+    binding.recycler.adapter = HomeScreenAdapter(context)
+    binding.recycler.layoutManager = LinearLayoutManager(context)
+    binding.contentsFoo.error.text = "foo"
   }
 
   var clickedRowIdx: Int? = null
@@ -32,10 +40,10 @@ class HomeScreenView(context: Context, attrs: AttributeSet?) : RecyclerView(cont
         .find { textView -> textView.tag == clickedRowIdx }
     }
 
-  inner class HomeScreenAdapter(private val context: Context) : Adapter<ViewHolder>() {
+  inner class HomeScreenAdapter(private val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val items = (0..80).toList()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
       val textView = TextView(context)
       textView.minHeight = 120
       textView.setPadding(40, 40, 40, 40)
@@ -45,7 +53,7 @@ class HomeScreenView(context: Context, attrs: AttributeSet?) : RecyclerView(cont
 
     override fun getItemCount(): Int = items.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
       (holder as RowViewHolder).set(items[position])
     }
 
