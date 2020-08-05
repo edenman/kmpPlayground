6c423e1b6246b46926587c27616e5980cb2531a3
Eric Denman <edenman@users.noreply.github.com>
2020-08-05 19:17:33 +1200

    move APIObjects to the correct sourceset

---
+++ shared/src/commonMain/kotlin/chat/quill/data/APIObjects.kt
@@ -0,0 +1,15 @@
+package chat.quill.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable(with = FoodSerializer::class)
+sealed class Food {
+  @Serializable
+  data class Taco(val taco: String) : Food()
+
+  @Serializable
+  data class Burrito(val burrito: String) : Food()
+  object Unknown : Food()
+}
+
+class FoodSerializer : OneOfSerializer<Food>(Food::class, Food.Unknown)
